987dc9bdade71a6c84a9704c3e8d38b384de3e46
spandanakoppaku <skoppaku@gmail.com>
2023-06-06 12:08:49 -0400

    shebag notation

--- 01.shebang.sh
+++ 01.shebang.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+## Start of the line in any bash script should always be a SHEBANG NOTATION.
+
+## Apart from the first line, none of the lines are considered as shebang
+## Shebang notatin should always start with #!
+
+
 echo welocme to Cloud devops 
 
 # Printing multiple lines
@@ -7,11 +14,17 @@ echo Line1
 echo Line2
 echo Line3
 
+#### How to run a linux bash script ? 
+
+    # *   bash scriptName.sh 
+    # *   sh scriptName.sh 
+    # *   ./scriptName
+
 # In bash, we have escape sequence characters, using that we can add some power to echo
 
-# /n  ; new line
-#/t   ; tab space
+# \n  ; new line
+# \t   ; tab space
 
-echo -e "line1/nline2"
-echo -e "line1/n/tline2"
+echo -e "line1\nline2"
+echo -e "line1\t\tline2"
 
