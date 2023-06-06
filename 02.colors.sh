514d139aaa58837286e20a55ff13f61bd61cd709
spandanakoppaku <skoppaku@gmail.com>
2023-06-06 12:17:27 -0400

    shebag notation

--- 02.colors.sh
+++ 02.colors.sh
@@ -17,3 +17,5 @@
  #      echo -e "\e[32m I am printing Green Color \e[0m"
 
  echo -e "\e[31m I am printing Red color \e[0m"
+ echo -e "\e[34m I am printing Green color \e[0m"
+ echo -e "\e[33m I am peinting yellow coloe \e[0m"
