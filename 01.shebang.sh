4ab8e22f4c6e6d9b9fe37127514dd88a862a1524
spandanakoppaku <skoppaku@gmail.com>
2023-06-06 12:04:44 -0400

    shebang notation

--- 01.shebang.sh
+++ 01.shebang.sh
@@ -7,3 +7,11 @@ echo Line1
 echo Line2
 echo Line3
 
+# In bash, we have escape sequence characters, using that we can add some power to echo
+
+# /n  ; new line
+#/t   ; tab space
+
+echo -e "line1/nline2"
+echo -e "line1/n/tline2"
+
