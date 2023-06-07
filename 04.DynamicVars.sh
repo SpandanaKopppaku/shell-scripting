45787bf95e0db91b32689ad102d07f99ac0f8fb3
spandanakoppaku <skoppaku@gmail.com>
2023-06-06 22:20:17 -0400

    shebag notation

--- 04.DynamicVars.sh
+++ 04.DynamicVars.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # TODAYS_DATE="03JUNE2023"  # Hardcoding a value makes it consistent
-TODAYS_DATE=date +%F
+TODAYS_DATE=$(date +%F)      # always enclose expressions in paranthesis
 echo "Good morning and todays date is ${TODAYS_DATE}"
