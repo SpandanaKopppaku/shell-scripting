c1e28323fad3cd7ada21c2a52011dfabf527dbff
spandanakoppaku <skoppaku@gmail.com>
2023-06-06 22:50:46 -0400

    shebag notation

--- 05.SpecialVaribale.sh
+++ 05.SpecialVaribale.sh
@@ -11,3 +11,12 @@ echo "Training Batch number is $2"
 echo "Current topic is $3"
 
 # we can supply the values from the command line
+
+# bash scriptName.sh 100   200   300  400  ( like this you can supply a maximum of 9 variables from the command line)
+#                     $1    $2    $3  $4
+
+echo $*    # $* is going to print the used variables  
+echo $@    # $@ is going to print the used variables  
+echo $$    # $$ is going to print the PID of the current proces 
+echo $#    # $# is going to pring the number of arguments
+echo $?    # $? is going to print the exit code of the last command.
