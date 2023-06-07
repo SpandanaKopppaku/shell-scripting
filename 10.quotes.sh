6be5e65d3e2649331358ea0c3bef456c400a6643
spandanakoppaku <skoppaku@gmail.com>
2023-06-07 00:19:09 -0400

    Auto Push

---
+++ 10.quotes.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# In bash, output of your command will be complete changes based on the quotes you use : 
+# ""   : Double Quotes 
+# ''   : Single Quotes 
+
+a=55
+echo "Value of a is ${a}
