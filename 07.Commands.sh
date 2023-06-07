e8af64288216e470071708a7cd8246ad7460e96e
spandanakoppaku <skoppaku@gmail.com>
2023-06-06 23:04:34 -0400

    shebag notation

--- 07.Commands.sh
+++ 07.Commands.sh
@@ -5,5 +5,5 @@
 
 # 1) Binary                   ( /bin  , /sbin )
 # 2) Aliases                  ( Alises are shortcuts,  alias net="netstat -tulpn" )
-# 3) Shell Built-in Commands  
+# 3) Shell Built-in Commands  ( Ex : type ls)
 # 4) Functions                # Functions are nothing but a set of command that can be written in a sequence and can be called n number of times as per your choice.
