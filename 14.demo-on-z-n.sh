23c2235cb78b775a11587b1868dff4bce7f1b7e8
spandanakoppaku <skoppaku@gmail.com>
2023-06-07 14:31:36 -0400

    Auto Push

--- 14.demo-on-z-n.sh
+++ 14.demo-on-z-n.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 
-# a=10
+a=10
 
-# if  [ -z "$a" ] ; then 
-#     echo "Value of a is declared"
-
-# else 
-#     echo "Value of a is not declared"
-
-# fi 
-
-if  [ -n "$a" ] ; then 
+if  [ -z "$a" ] ; then 
     echo "Value of a is declared"
 
 else 
     echo "Value of a is not declared"
 
 fi 
+
+# if  [ -n "$a" ] ; then 
+#     echo "Value of a is declared"
+
+# else 
+#     echo "Value of a is not declared"
+
+# fi 
