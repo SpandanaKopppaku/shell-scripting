72122a1a4d29f01ee1f0dc34581386c8f68610be
spandanakoppaku <skoppaku@gmail.com>
2023-06-07 23:55:40 -0400

    Auto Push

--- Roboshop/components/mongodb.sh
+++ Roboshop/components/mongodb.sh
@@ -8,25 +8,25 @@ echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m **********
 
 echo -n "Configuring the $COMPONENT repo:"
 
-curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo   &>> $LOGFILE
+curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo   
 stat $?
 
 echo -n "Installing the $COMPONENT:"
-yum install -y ${COMPONENT}-org         &>> $LOGFILE
+yum install -y ${COMPONENT}-org         
 stat $?
 
 echo -n "Enabling the DB visibility:"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf       &>> $LOGFILE
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf       
 stat $?
 
 echo -n "Starting the ${COMPONENT} Service: "
-systemctl daemon-reload mongod      &>> $LOGFILE
-systemctl enable mongod             &>> $LOGFILE
-systemctl restart mongod            &>> $LOGFILE
+systemctl daemon-reload mongod      
+systemctl enable mongod             
+systemctl restart mongod            
 stat $?
 
 echo -n " Downloading the ${COMPONENT} schema:"
-curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"      &>> $LOGFILE
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"      
 stat $?
 
 echo -n "Extracting the $COMPONENT Schema:"
