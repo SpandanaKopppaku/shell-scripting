10f5f9360c8361353611ac2569fdb3c69d9703ef
spandanakoppaku <skoppaku@gmail.com>
2023-06-07 23:28:44 -0400

    Auto Push

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -6,15 +6,15 @@ LOGFILE="/tmp/${COMPONENT}.log"
 
 echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m ***********"
 
-if [ $ID -ne 0 ] ; then
+if [ $ID -ne 0 ] ; then     &>> $LOGFILE
 
-    echo -e "\e[31m This script is expected to be run by a root user or with a sudo privilege \e[0m"
+    echo -e "\e[31m This script is expected to be run by a root user or with a sudo privilege \e[0m" 
     exit 1
 
 fi
 
 stat() {
-    if [ $1 -eq 0 ] ; then 
+    if [ $1 -eq 0 ] ; then      &>> $LOGFILE
         echo -e "\e[32m success \e[0m"
     else 
         echo -e "\e[31m failure \e[0m"
