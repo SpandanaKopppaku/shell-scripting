c81cf6bc55fb34ef957b015cf18dadade5366b2e
spandanakoppaku <skoppaku@gmail.com>
2023-06-07 23:44:44 -0400

    Auto Push

--- Roboshop/components/mongodb.sh
+++ Roboshop/components/mongodb.sh
@@ -8,7 +8,7 @@ echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m **********
 
 echo -n "Configuring the $COMPONENT repo:"
 
-curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo   &>> $LOGFILE
+curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo   &>> $LOGFILE
 stat$?
 
 echo -n "Installing the $COMPONENT:"
@@ -16,4 +16,28 @@ yum install -y ${COMPONENT}-org
 stat$?
 
 echo -n "Enabling the DB visibility:"
-sed -i -e 's/127.0.0.1/0.0.0.0/ /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+stat$?
+
+echo -n "Starting the ${COMPONENT} Service: "
+systemctl daemon-reload mongod
+systemctl enable mongod
+systemctl restart mongod
+stat$?
+
+echo -n " Downloading the ${COMPONENT} schema:"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+stat$?
+
+echo -n "Extracting the $COMPONENT Schema:"
+cd /tmp
+unzip -o ${COMPONENT}.zip
+stat$?
+
+echo -n "Injecting the schema:"
+cd ${COMPONENT}-main
+mongo < catalogue.js
+mongo < users.js
+stat$?
+
+echo -e "*********** \e[35m $COMPONENT Installation is completed \e[0m ***********"
