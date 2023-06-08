e8352402c9be93c6f7e421f7fa123bef2c04d951
spandanakoppaku <skoppaku@gmail.com>
2023-06-07 23:16:25 -0400

    Auto Push

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -2,6 +2,8 @@
 
 COMPONENT=frontend
 
+LOGFILE="/tmp/${COMPONENT}.log"
+
 echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m ***********"
 
 if [ $ID -ne 0 ] ; then
@@ -20,32 +22,32 @@ stat() {
     fi 
 }
 
-echo -n " Installing Nginx: "
+echo -n " Installing Nginx: "  &>> $LOGFILE
 
 yum install nginx -y
 stat $?
 
 echo -n " Downloading the ${COMPONENT} component: "
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"      &>> $LOGFILE
 stat $?
 
 echo -n "Performing cleanup"
 cd /usr/share/nginx/html
-rm -rf *
+rm -rf *  &>> $LOGFILE
 stat $?
 
 echo -n "Extracting ${COMPONENT} component :"
-unzip /tmp/${COMPONENT}.zip
+unzip /tmp/${COMPONENT}.zip         &>> $LOGFILE
 mv ${COMPONENT}-main/* .
 mv static/* .
 rm -rf ${COMPONENT}-main README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+mv localhost.conf /etc/nginx/default.d/roboshop.conf        
 stat $?
 
 echo -n "Starting $COMPONENT service: "
-Systemctl daemon-reload
-systemctl enable nginx
-systemctl start nginx
+Systemctl daemon-reload         &>> $LOGFILE
+systemctl enable nginx          &>> $LOGFILE
+systemctl start nginx           &>> $LOGFILE
 stat $?
 
 
