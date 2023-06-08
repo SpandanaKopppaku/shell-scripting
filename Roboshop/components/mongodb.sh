10d8a76583a833084e9086061d2fc532ec98c671
spandanakoppaku <skoppaku@gmail.com>
2023-06-07 23:37:46 -0400

    Auto Push

--- Roboshop/components/mongodb.sh
+++ Roboshop/components/mongodb.sh
@@ -5,3 +5,15 @@ COMPONENT=mongodb
 LOGFILE="/tmp/${COMPONENT}.log"
 
 echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m ***********"
+
+echo -n "Configuring the $COMPONENT repo:"
+
+curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo   &>> $LOGFILE
+stat$?
+
+echo -n "Installing the $COMPONENT:"
+yum install -y ${COMPONENT}-org
+stat$?
+
+echo -n "Enabling the DB visibility:"
+sed -e 's/127.0.0.1/0.0.0.0/ /etc/mongod.conf
