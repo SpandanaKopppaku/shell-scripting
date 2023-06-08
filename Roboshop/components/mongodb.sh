ff95013b66656c3ccd366a4c894f28eeb5462df9
spandanakoppaku <skoppaku@gmail.com>
2023-06-08 00:16:30 -0400

    Auto Push

--- Roboshop/components/mongodb.sh
+++ Roboshop/components/mongodb.sh
@@ -2,26 +2,7 @@
 
 COMPONENT=mongodb
 
-LOGFILE="/tmp/${COMPONENT}.log"
-
-echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m ***********"
-
-if [ $ID -ne 0 ] ; then     &>> $LOGFILE
-
-    echo -e "\e[31m This script is expected to be run by a root user or with a sudo privilege \e[0m" 
-    exit 1
-
-fi
-
-stat() {
-    if [ $1 -eq 0 ] ; then      &>> $LOGFILE
-        echo -e "\e[32m success \e[0m"
-    else 
-        echo -e "\e[31m failure \e[0m"
-        exit 2
-    fi 
-}
-
+source components/common.sh
 
 echo -n  "Configuring the $COMPONENT repo :"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
