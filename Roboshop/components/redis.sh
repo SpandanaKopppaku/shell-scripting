1ad2ffbc512064cc65d7be8a1aa2ab075115ca17
spandanakoppaku <skoppaku@gmail.com>
2023-06-08 23:59:47 -0400

    Auto Push

--- Roboshop/components/redis.sh
+++ Roboshop/components/redis.sh
@@ -5,20 +5,20 @@ COMPONENT=redis
 source components/common.sh
 
 echo -n "Downloading the redis repo:"
-curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo      &>> $LOGFILE
 stat $?
 
 echo -n "Installing the redis repo:"
-yum install redis-6.2.11 -y
+yum install redis-6.2.11 -y     &>> $LOGFILE
 stat $?
 
 echo -n "Enabling the DB visibility :"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf   
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf   &>> $LOGFILE
 stat $? 
 
 echo -n "Starting $COMPONENT service: "
-systemctl daemon-reload         &>> $LOGFILE
-systemctl enable $COMPONENT          &>> $LOGFILE
+systemctl daemon-reload             &>> $LOGFILE
+systemctl enable $COMPONENT         &>> $LOGFILE
 systemctl start $COMPONENT          &>> $LOGFILE
 stat $?
 
