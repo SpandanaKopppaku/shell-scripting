43f9a03d30160fdb63f1e6a549debbb43aa70e41
spandanakoppaku <skoppaku@gmail.com>
2023-06-09 00:22:49 -0400

    Auto Push

--- Roboshop/components/catalogue.sh
+++ Roboshop/components/catalogue.sh
@@ -15,19 +15,7 @@ yum install nodejs -y       &>> $LOGFILE
 stat $?
 
 
-echo -n "Switching the User from root to roboshop"
-chown -R $APPUSER:$APPUSER /home/roboshop/$COMPONENT/
-
-echo -n "Downloading the $COMPONENT component :"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"   &>> $LOGFILE
-stat $?
-
-echo -n "Copying the $COMPONENT to $APPUSER home directory :"
-cd /home/roboshop   &>> $LOGFILE
-rm -rf $COMPONENT   &>> $LOGFILE
-unzip -o /tmp/catalogue.zip        &>> $LOGFILE
-mv catalogue-main catalogue     &>> $LOGFILE
-stat $?
+DOWNLOAD_AND_EXTRACT
 
 echo -n "Generating npm $COMPONENT artifacts :"
 cd /home/roboshop/catalogue     &>> $LOGFILE
--- Roboshop/components/user.sh
+++ Roboshop/components/user.sh
@@ -15,19 +15,7 @@ yum install nodejs -y
 
 CREATE_USER
 
-echo -n "Switching the User from root to roboshop"
-chown -R $APPUSER:$APPUSER /home/roboshop/$COMPONENT/
-
-echo -n "Downloading the $COMPONENT component :"
-curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"   &>> $LOGFILE
-stat $?
-
-echo -n "Copying the $COMPONENT to $APPUSER home directory :"
-cd /home/roboshop   &>> $LOGFILE
-rm -rf $COMPONENT   &>> $LOGFILE
-unzip -o /tmp/$COMPONENT.zip        &>> $LOGFILE
-mv $COMPONENT-main $COMPONENT    &>> $LOGFILE
-stat $?
+DOWNLOAD_AND_EXTRACT
 
 echo -n "Generating npm $COMPONENT artifacts :"
 cd /home/roboshop/$COMPONENT     &>> $LOGFILE
