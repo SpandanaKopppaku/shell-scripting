60e8ebe6f1a556ddb12a2be9553c5ffc2ecd066d
spandanakoppaku <skoppaku@gmail.com>
2023-06-08 23:58:37 -0400

    Auto Push

--- Roboshop/components/redis.sh
+++ Roboshop/components/redis.sh
@@ -6,6 +6,12 @@ source components/common.sh
 
 echo -n "Downloading the redis repo:"
 curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+stat $?
 
 echo -n "Installing the redis repo:"
 yum install redis-6.2.11 -y
+stat $?
+
+echo -n "Enabling the DB visibility :"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf   
+stat $? 
