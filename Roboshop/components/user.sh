77a95fc8fdd71a2e389097320912a8de57883559
spandanakoppaku <skoppaku@gmail.com>
2023-06-09 00:11:14 -0400

    Auto Push

---
+++ Roboshop/components/user.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+COMPONENT=user
+
+source components/common.sh
+
+echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m ***********"
+
+echo -e "Downloading the NodeJs repo:"
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+stat $?
+
+echo -n "Installing NodeJS:"
+yum install nodejs -y
