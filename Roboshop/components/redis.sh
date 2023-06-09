20655c8f3c000bd0e8910a64ba02bd3aa871bd49
spandanakoppaku <skoppaku@gmail.com>
2023-06-08 23:57:27 -0400

    Auto Push

--- Roboshop/components/redis.sh
+++ Roboshop/components/redis.sh
@@ -8,3 +8,4 @@ echo -n "Downloading the redis repo:"
 curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
 
 echo -n "Installing the redis repo:"
+yum install redis-6.2.11 -y
