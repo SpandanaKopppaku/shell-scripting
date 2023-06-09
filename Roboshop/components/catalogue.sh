f738dc74de95f2b62f845421aa35c74a5beed170
spandanakoppaku <skoppaku@gmail.com>
2023-06-08 22:40:59 -0400

    Auto Push

--- Roboshop/components/catalogue.sh
+++ Roboshop/components/catalogue.sh
@@ -28,11 +28,11 @@ echo -n "Downloading the $COMPONENT component :"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"   &>> $LOGFILE
 cd /home/roboshop
 
-cd /home/roboshop
-unzip /tmp/catalogue.zip
-mv catalogue-main catalogue
-cd /home/roboshop/catalogue
-npm install
+cd /home/roboshop   &>> $LOGFILE
+unzip /tmp/catalogue.zip        &>> $LOGFILE
+mv catalogue-main catalogue     &>> $LOGFILE
+cd /home/roboshop/catalogue     &>> $LOGFILE
+npm install                     &>> $LOGFILE
 
 
 
