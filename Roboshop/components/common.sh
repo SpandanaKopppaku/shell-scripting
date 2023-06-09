53099c27b43aafa124848a449ce089f582ade5f4
spandanakoppaku <skoppaku@gmail.com>
2023-06-08 20:35:57 -0400

    Auto Push

--- Roboshop/components/common.sh
+++ Roboshop/components/common.sh
@@ -5,7 +5,7 @@ LOGFILE="/tmp/${COMPONENT}.log"
 
 echo -e "*********** \e[35m $COMPONENT Installation has started \e[0m ***********"
 
-if [ $ID -ne 0 ] ; then     &>> $LOGFILE
+if [ $ID -ne 0 ] ; then      &>> $LOGFILE
 
     echo -e "\e[31m This script is expected to be run by a root user or with a sudo privilege \e[0m" 
     exit 1
