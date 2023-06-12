dbac6b1ac88342e44dd1e10a1a5c591859fb4850
spandanakoppaku <skoppaku@gmail.com>
2023-06-12 18:04:25 -0400

    Auto Push

--- Roboshop/create-ec2.sh
+++ Roboshop/create-ec2.sh
@@ -12,4 +12,4 @@ echo -e "Security Group ID used to launch the EC2 is \e[35m $SG_ID \e[0m"
 echo -e "\e[36m **** Launching Server **** \e[0m"
 
 
-aws ec2 run-instances  --image-id ${AMI_ID}  --instance-type t3.micro --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=${COMPONENT}}]"
+aws ec2 run-instances  --image-id ${AMI_ID}  --instance-type t3.micro --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$COMPONENT}]"
