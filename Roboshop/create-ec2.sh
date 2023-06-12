64985125013fe3d97251608a8073fce65b2287e1
spandanakoppaku <skoppaku@gmail.com>
2023-06-12 17:46:09 -0400

    Auto Push

--- Roboshop/create-ec2.sh
+++ Roboshop/create-ec2.sh
@@ -3,9 +3,10 @@
 # AMI_ID="ami-0c1d144c8fdd8d690"
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId'|sed -e 's/"//g')
+SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b54-allow-all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
 echo -e "AMI ID used to launch the EC2 is \e[35m $AMI_ID \e[0m"
 
-echo -e " Launching the Instance:"
+echo -e "*******Launching the server ************"
 
 aws ec2 run-instances  --image-id ${AMI_ID}  --instance-type t3.micro
